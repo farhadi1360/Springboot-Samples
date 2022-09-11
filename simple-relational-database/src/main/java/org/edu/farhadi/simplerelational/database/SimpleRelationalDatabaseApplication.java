c623fed2fe9a3c37bfb025c1b59b5f814cbde333
farhadi1360 <farhadi.kam@gmail.com>
2022-09-11 14:30:06 +0430

    first commit

---
+++ simple-relational-database/src/main/java/org/edu/farhadi/simplerelational/database/SimpleRelationalDatabaseApplication.java
@@ -0,0 +1,13 @@
+package org.edu.farhadi.simplerelational.database;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SimpleRelationalDatabaseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SimpleRelationalDatabaseApplication.class, args);
+	}
+
+}
---
+++ simple-relational-database/src/test/java/org/edu/farhadi/simplerelational/database/SimpleRelationalDatabaseApplicationTests.java
@@ -0,0 +1,13 @@
+package org.edu.farhadi.simplerelational.database;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SimpleRelationalDatabaseApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
